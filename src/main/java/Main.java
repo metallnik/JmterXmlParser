31bed11aaaf332d51bd9085c128c124aa68c1152
metallnik <kuleshov_w@mail.ru>
2020-05-29 22:16:11 +0300

    deleted throws from psvm method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //       FileInputStream fileInputStream =  new FileInputStream("C:\\Users\\vipku\\IdeaProjects\\xmlparser\\src\\main\\resources\\test.xml");
         try (FileInputStream fileInputStream = new FileInputStream("C:\\Users\\vipku\\OneDrive\\Desktop\\LoadTest\\result128.xml")) {
             try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("C:\\Users\\vipku\\OneDrive\\Desktop\\LoadTest\\newResult.txt"))) {
